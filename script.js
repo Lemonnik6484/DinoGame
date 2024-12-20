c02a1e8f2370634c2c549c520e0ce59e2c237837
Lemonnik6484 <lemongamer6484@gmail.com>
2024-12-20 12:40:32 +0100

    better jump and pterodactyl

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const dino = document.getElementById('dino');
 const game = document.body;
 let scoreText = document.getElementById('score');
+const gravity = 0.75;
+const jumpForce = 15;
+const groundLevel = 22;
+let dinoY = 22;
 let isJumping = false;
+let velocity = 0;
 let cactusPositions = [];
 let score = 0;
+
+function update() {
+    velocity -= gravity;
+    dinoY += velocity
+
+    if (dinoY <= groundLevel) {
+        dinoY = groundLevel;
+        velocity = 0;
+        isJumping = false;
+    }
+
+    dino.style.bottom = `${dinoY}px`;
+
+    requestAnimationFrame(update);
+}
 
 document.addEventListener('keydown', (e) => {
     if (e.code === 'Space' && !isJumping) {
         isJumping = true;
-        dino.style.bottom = '125px';
-
-        setTimeout(() => {
-            dino.style.bottom = '22px';
-            setTimeout(() => {
-                isJumping = false;
-            }, 200)
-        }, 350) // jump time
+        velocity = jumpForce;
     }
 })
 
@@ -69,4 +82,60 @@ function spawnCactus() {
     spawnNewCactus();
 }
 
+function spawnCloud() {
+    const cloud = document.createElement('div');
+    const size = Math.random() * 50 + 50;
+    const speed = (150 - size) / 20;
+
+    cloud.classList.add('cloud');
+    cloud.style.width = `${size}px`;
+    cloud.style.height = `${size/2}px`;
+    cloud.style.top = `${Math.random() * 50}px`
+    cloud.style.animationDuration = `${speed}s`
+    cloud.style.backgroundSize = `${size}px ${size/2}px`;
+    cloud.style.zIndex = `${size}`;
+
+    game.appendChild(cloud);
+
+    setTimeout(() => {
+        cloud.remove();
+    }, speed * 1000);
+
+    setTimeout(spawnCloud, Math.random() * 2000 + 500);
+}
+
+function spawnPter () {
+    const pter = document.createElement('div');
+
+    pter.classList.add('pterodactyl');
+    pter.style.right = "0px";
+
+    game.appendChild(pter);
+
+    const pterInterval = setInterval(() => {
+        const dinoRect = dino.getBoundingClientRect();
+        const pterRect = pter.getBoundingClientRect();
+
+        if (dinoRect.right > pterRect.left && dinoRect.left < pterRect.right && dinoRect.bottom > pterRect.top) {
+            dino.classList.add('dead');
+            clearInterval(pterInterval);
+            alert("Game over!");
+            location.reload();
+        }
+
+        if (pterRect.right <= 0) {
+            pter.remove();
+            clearInterval(pterInterval);
+            setTimeout(() => {
+                spawnPter()
+            }, Math.random() * 15000 + 750);
+        }
+    }, 50);
+}
+
+spawnCloud();
 spawnCactus();
+setTimeout(() => {
+    spawnPter();
+}, Math.random() * 25000 + 10000);
+update();
